07e3d096f221eebf811b71527e558faf77b97d17
Christina Esico <christinaesico456@gmail.com>
2025-11-20 00:34:14 +0800

    Added Timeout

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://verifeed-back
 // ========================
 const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 15000,
+  timeout: 30000,
   headers: { "Content-Type": "application/json" },
 })
 
